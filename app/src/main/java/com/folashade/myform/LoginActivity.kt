b1f9b1531f59f2a3cf11a821f94d0ba508f1f268
omoloye13 <omoloyefauziyat@gmail.com>
2023-11-26 20:21:04 +0000

    added validations to the LoginActivity

--- app/src/main/java/com/folashade/myform/LoginActivity.kt
+++ app/src/main/java/com/folashade/myform/LoginActivity.kt
@@ -2,10 +2,49 @@ package com.folashade.myform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var createNAButton: Button = findViewById(R.id.createNewAccount)
+
+        createNAButton.setOnClickListener{
+            val intent = Intent (this, CreateNewAccountActivity::class.java)
+            this.startActivity(intent)
+        }
+        val loginButton :Button = this.findViewById(R.id.loginButton)
+        val userName : EditText = this.findViewById(R.id.edit_text1)
+        val password : EditText = this.findViewById(R.id.edit_text2)
+
+         fun validateUserInput(){
+            val enteredUserName : String = userName.text.toString()
+            val enteredPassword : String = password.text.toString()
+             val message = "You need to enter your username and password";
+
+
+             if (enteredUserName.isNotEmpty() && enteredPassword.isNotEmpty()){
+                 val navigation = Intent ( this, HomeActivity::class.java)
+                 this.startActivity(navigation)
+             } else{
+                 Snackbar
+                     .make(findViewById(R.id.content),
+                         message,
+                         Snackbar.LENGTH_LONG
+                         )
+                     .show()
+
+
+             }
+        }
+        loginButton.setOnClickListener {
+
+            validateUserInput()
+        }
     }
 }
